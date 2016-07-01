b8060b81c0a7ffeec4f946723bae5c507a5edd45
JohnH <john.harrison@intel.com>
2016-06-30 17:51:09 -0700

    Done with the assignment

---
+++ CSharpScratchpad/CSharpScratchpad/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpScratchpad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 1;
+            double b = (x - y) / (double)x;
+        }
+    }
+}
